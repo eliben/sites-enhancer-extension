3746f9ce875c962573d636ee586d8c366c0d49a4
Eli Bendersky <eliben@chromium.org>
2013-12-10 10:06:25 -0800

    yay - managed to apply code formatting

--- content.js
+++ content.js
@@ -1,9 +1,33 @@
 console.log("hello from content script");
 
+// Generic JS utility to get currently selected text from a document.
+function getSelectionHtml() {
+  var html = "";
+  if (typeof window.getSelection != "undefined") {
+    var sel = window.getSelection();
+    if (sel.rangeCount) {
+      var container = document.createElement("div");
+      for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+        container.appendChild(sel.getRangeAt(i).cloneContents());
+      }
+      html = container.innerHTML;
+    }
+  } else if (typeof document.selection != "undefined") {
+    if (document.selection.type == "Text") {
+      html = document.selection.createRange().htmlText;
+    }
+  }
+  return html;
+}
+
+// Event listener for messages sent to us
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+  // Formatting messages from the background script
   if (request.action === 'apply-format-code') {
     console.log('Got message:', request);
-    document.execCommand('fontName', false, 'courier');
+    console.log('Current selection is', getSelectionHtml());
+    var newtext = '<code>' + getSelectionHtml() + '</code>'
+    document.execCommand('insertHTML', false, newtext);
   }
 });
 
